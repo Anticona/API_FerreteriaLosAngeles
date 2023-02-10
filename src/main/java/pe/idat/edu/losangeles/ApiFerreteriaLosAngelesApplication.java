5d250986f17ab09c3ebe315a4f5e7aa62f06f117
Anticona <ln72550352@idat.edu.pe>
2023-02-09 20:30:13 -0500

    Creación de Proyecto
    Inicio de Proyecto

---
+++ src/main/java/pe/idat/edu/losangeles/ApiFerreteriaLosAngelesApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.losangeles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFerreteriaLosAngelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFerreteriaLosAngelesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/idat/edu/losangeles/ApiFerreteriaLosAngelesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.edu.losangeles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFerreteriaLosAngelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
