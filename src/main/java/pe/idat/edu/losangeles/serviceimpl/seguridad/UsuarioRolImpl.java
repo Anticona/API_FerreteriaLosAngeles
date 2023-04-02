ae63e867bf52002bf5c3ee9a6c4ad515e38ba122
Anticona <ln72550352@idat.edu.pe>
2023-04-01 20:30:18 -0500

    service impl de seguridad

---
+++ src/main/java/pe/idat/edu/losangeles/serviceimpl/seguridad/UsuarioRolImpl.java
@@ -0,0 +1,61 @@
+
+package pe.idat.edu.losangeles.serviceimpl.seguridad;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.idat.edu.losangeles.entity.seguridad.UsuarioRolEntity;
+import pe.idat.edu.losangeles.repository.seguridad.UsuarioRolRepository;
+import pe.idat.edu.losangeles.service.seguridad.UsuarioRolService;
+
+/**
+ *
+ * @author Sebastian
+ */
+@Service
+public class UsuarioRolImpl implements UsuarioRolService{
+
+    @Autowired
+     private UsuarioRolRepository repositorio;
+
+    @Override
+    public List<UsuarioRolEntity> findAll() {
+      return repositorio.findAll();        
+    }
+
+    
+    @Override
+    public UsuarioRolEntity add(UsuarioRolEntity u) {
+        return repositorio.save(u);
+    }
+
+    @Override
+    public Optional<UsuarioRolEntity> findById(Long id) {
+        return repositorio.findById(id);
+    }
+
+    @Override
+    public UsuarioRolEntity update(UsuarioRolEntity u) {
+        UsuarioRolEntity objusuariorol=repositorio.getById(u.getIdusuariorol());
+        BeanUtils.copyProperties(u, objusuariorol);
+        return repositorio.save(objusuariorol);
+    }
+
+    @Override
+    public List<UsuarioRolEntity> findAllCustom() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public UsuarioRolEntity delete(UsuarioRolEntity t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public UsuarioRolEntity enable(UsuarioRolEntity t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
---
+++ src/main/java/pe/idat/edu/losangeles/serviceimpl/seguridad/UsuarioServiceImpl.java
@@ -0,0 +1,64 @@
+
+package pe.idat.edu.losangeles.serviceimpl.seguridad;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.idat.edu.losangeles.entity.seguridad.UsuarioEntity;
+import pe.idat.edu.losangeles.repository.seguridad.UsuarioRepository;
+import pe.idat.edu.losangeles.service.seguridad.UsuarioService;
+
+/**
+ *
+ * @author Sebastian
+ */
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+    @Autowired
+     private UsuarioRepository repositorio;
+
+    @Override
+    public List<UsuarioEntity> findAll() {
+      return repositorio.findAll();        
+    }
+
+    @Override
+    public List<UsuarioEntity> findAllCustom() {
+        return repositorio.findAllCustom();
+    }
+    
+    @Override
+    public UsuarioEntity add(UsuarioEntity u) {
+        return repositorio.save(u);
+    }
+
+    @Override
+    public Optional<UsuarioEntity> findById(Long id) {
+        return repositorio.findById(id);
+    }
+
+    @Override
+    public UsuarioEntity update(UsuarioEntity u) {
+        UsuarioEntity objusuario=repositorio.getById(u.getIdusuario());
+        BeanUtils.copyProperties(u, objusuario);
+        return repositorio.save(objusuario);
+    }
+
+    @Override
+    public UsuarioEntity delete(UsuarioEntity u) {
+        UsuarioEntity objusuario=repositorio.getById(u.getIdusuario());
+        objusuario.setEstado(false);
+        return repositorio.save(objusuario);
+    }
+
+    @Override
+    public UsuarioEntity enable(UsuarioEntity u) {
+        UsuarioEntity objusuario=repositorio.getById(u.getIdusuario());
+        objusuario.setEstado(true);
+        return repositorio.save(objusuario);
+    }
+
+}
