839c3375fe0f78fe7cb6760c7f3c57830234aa53
Anticona <ln72550352@idat.edu.pe>
2023-04-01 20:30:08 -0500

    paquete service de seguridad

---
+++ src/main/java/pe/idat/edu/losangeles/service/seguridad/RolService.java
@@ -0,0 +1,15 @@
+
+
+package pe.idat.edu.losangeles.service.seguridad;
+
+import pe.idat.edu.losangeles.entity.seguridad.RolEntity;
+import pe.idat.edu.losangeles.service.generico.GenericoService;
+
+/**
+ *
+ * @author Sebastian
+ */
+
+public interface RolService extends GenericoService<RolEntity>{
+
+}
---
+++ src/main/java/pe/idat/edu/losangeles/service/seguridad/UsuarioRolService.java
@@ -0,0 +1,13 @@
+
+
+package pe.idat.edu.losangeles.service.seguridad;
+
+import pe.idat.edu.losangeles.entity.seguridad.UsuarioRolEntity;
+import pe.idat.edu.losangeles.service.generico.GenericoService;
+
+/**
+ *
+ * @author Sebastian
+ */
+public interface UsuarioRolService extends GenericoService<UsuarioRolEntity>{
+}
