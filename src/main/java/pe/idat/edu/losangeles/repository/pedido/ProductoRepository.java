9d3333bb844573ff61bc23e3727778566c14de63
Anticona <ln72550352@idat.edu.pe>
2023-04-02 21:54:17 -0500

    @repository

--- src/main/java/pe/idat/edu/losangeles/repository/pedido/ProductoRepository.java
+++ src/main/java/pe/idat/edu/losangeles/repository/pedido/ProductoRepository.java
@@ -4,6 +4,7 @@ package pe.idat.edu.losangeles.repository.pedido;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import pe.idat.edu.losangeles.entity.pedido.ProductoEntity;
 import pe.idat.edu.losangeles.repository.generico.GenericoRepository;
 
@@ -11,6 +12,7 @@ import pe.idat.edu.losangeles.repository.generico.GenericoRepository;
  *
  * @author Sebastian
  */
+@Repository
 public interface ProductoRepository extends GenericoRepository<ProductoEntity, Long>{
     @Query("select p from ProductoEntity p where p.estado=1")
     List<ProductoEntity> findAllCustom();
