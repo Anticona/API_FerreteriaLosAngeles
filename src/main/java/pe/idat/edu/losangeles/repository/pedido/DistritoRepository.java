c6b18100c2682aa6659a4cdfa7fd0c765acfc482
Anticona <ln72550352@idat.edu.pe>
2023-04-02 21:54:33 -0500

    Update DistritoRepository.java

--- src/main/java/pe/idat/edu/losangeles/repository/pedido/DistritoRepository.java
+++ src/main/java/pe/idat/edu/losangeles/repository/pedido/DistritoRepository.java
@@ -4,6 +4,7 @@ package pe.idat.edu.losangeles.repository.pedido;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import pe.idat.edu.losangeles.entity.pedido.DistritoEntity;
 import pe.idat.edu.losangeles.repository.generico.GenericoRepository;
 
@@ -11,6 +12,7 @@ import pe.idat.edu.losangeles.repository.generico.GenericoRepository;
  *
  * @author Sebastian
  */
+@Repository
 public interface DistritoRepository extends GenericoRepository<DistritoEntity, Long>{
     @Query("select d from DistritoEntity d where d.estado=1")
     List<DistritoEntity> findAllCustom();
