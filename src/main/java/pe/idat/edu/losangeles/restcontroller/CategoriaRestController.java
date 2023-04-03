185bfe2d054be2737a08c9ab57ca20a3a95d8ed8
Anticona <ln72550352@idat.edu.pe>
2023-04-02 19:35:54 -0500

    categoriaRestController

--- src/main/java/pe/idat/edu/losangeles/restcontroller/CategoriaRestController.java
+++ src/main/java/pe/idat/edu/losangeles/restcontroller/CategoriaRestController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.idat.edu.losangeles.entity.dto.pedido.CategoriaDTO;
-import pe.idat.edu.losangeles.mapper.pedido.CategoriaMapper;
 import pe.idat.edu.losangeles.service.pedido.CategoriaService;
 
 /**
@@ -25,8 +24,6 @@ public class CategoriaRestController {
 
 @Autowired
     private CategoriaService servicio;
-
-    private CategoriaMapper mapper;
 
     @GetMapping
     public List<CategoriaDTO> findAll() {
